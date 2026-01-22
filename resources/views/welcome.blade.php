d7cd5f928e5513561afeccf30a9dec1a71ace154
Radhiwidya <Radhiwidya@users.noreply.github.com>
2026-01-22 22:07:11 +0700

    add tailwind

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    <h2 class="text-2xl text-blue-700 font-bold italic">Hello world</h2>
+</body>
+
+</html>
